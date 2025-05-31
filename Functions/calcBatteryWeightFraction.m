60100ef654a98b56ac72ef997666fdacd559ad8f
DESKTOP-95332UT\nate <nathen9752@gmail.com>
2025-05-31 12:59:20 -0700

    took calcBatteryWeight fraction from senior design and made small changes for robustness

--- Functions/calcBatteryWeightFraction.m
+++ Functions/calcBatteryWeightFraction.m
@@ -1,4 +1,4 @@
-function  aircraft = calcBatteryWeightFraction(aircraft,engine,mission)
+function  aircraft = calcBatteryWeightFraction(aircraft,mission)
 %weightEstimation 
 %   This code will develop an energy map of the mission and use this to
 %   size the weight of the aircraft and the weight of the battery. 
@@ -6,34 +6,34 @@ function  aircraft = calcBatteryWeightFraction(aircraft,engine,mission)
 fprintf('\n--------Weight Fraction Estimations--------\n\n')
 
 % general unpackaging
-gamma = aircraft.gamma;
-V_cruise = aircraft.V_cruise;
-V_climb = aircraft.V_climb;
-h_cruise = mission.cruise_alt;
-LD = aircraft.LD;
-rhob = engine.rhob;
-eta_p = engine.eta_p;
-eta_m = engine.eta_m;
-straight = mission.straight;
-turnradius = mission.turnradius; 
-numTurns = mission.numLaps;
+gamma = aircraft.aero.gamma;
+V_cruise = aircraft.aero.V_cruise;
+V_climb = aircraft.aero.V_climb;
+cruise_alt = aircraft.aero.cruiseAlt;
+LD = aircraft.aero.LD;
+rho_b = aircraft.engine.rho_b;
+eta_p = aircraft.engine.eta_p;
+eta_m = aircraft.engine.eta_m;
+
+cruiseDuration = mission.cruiseDuration;
 
 % Energy consumption in level flight (Wb/W) 
-straight_distance = straight * numTurns * 2;   % three laps, two straightaways
-t_cruise = straight_distance / V_cruise;
-fprintf('Time in cruise: %.2f seconds.\n',t_cruise);
-WBlf_W = 1.356*V_cruise*t_cruise / (eta_p*eta_m*LD*rhob);
+fprintf('Time in cruise: %.2f seconds.\n',cruiseDuration);
+WBlf_W = 1.356*V_cruise*cruiseDuration / (eta_p*eta_m*LD*rho_b);   % weight fraction level flight
 
 % Energy consumption turning flight (Wb/W)
-t_turning = numTurns*2*pi*turnradius / V_cruise; 
-fprintf('Time in turns: %.2f seconds.\n',t_turning);
-WBtu_W = 1.356 * V_cruise * t_turning / (LD*eta_p*eta_m*rhob); 
+WBtu_W = 0;
+if mission.turning.flag 
+    t_turning = numTurns*2*pi*turnradius / V_cruise; 
+    fprintf('Time in turns: %.2f seconds.\n',t_turning);
+    WBtu_W = 1.356 * V_cruise * t_turning / (LD*eta_p*eta_m*rho_b); 
+end
 
 % Energy consumption in climbing flight (Wb/W)
 V_up = sind(gamma)*V_climb;
-t_climb = h_cruise/V_up;   % seconds 
-fprintf('Time in climb: %.2f seconds.\n\n',t_climb);
-WBcl_W = 1.356*V_climb*t_climb / (eta_p*eta_m*rhob) *...
+climbDuration = cruise_alt/V_up;   % seconds 
+fprintf('Time in climb: %.2f seconds.\n\n',climbDuration);
+WBcl_W = 1.356*V_climb*climbDuration / (eta_p*eta_m*rho_b) *...
                                  (cosd(gamma)/LD + sind(gamma));
 
 % Energy consumption warmup and takeoff (Wb/W)
@@ -50,7 +50,7 @@ fprintf('Weight Fraction Buildup: \n');
 fprintf('1/W * [WB = WBto + WBwu + WBcl + WBlf + WBtu]\n');
 fprintf('%.4f + %.4f + %.4f + %.4f + %.4f = %.4f\n\n',WBto_W,WBwu_W,WBcl_W,WBlf_W,WBtu_W,WB_W);
 
-aircraft.WB_W = WB_W;
+aircraft.weeight.WB_W = WB_W;
 
 end
 
