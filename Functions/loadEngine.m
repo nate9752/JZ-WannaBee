7a5f4b2d23dad63aaea1a5604f3c5383384247f3
Nathen Carey <nathen@jetzero.aero>
2025-06-05 16:13:18 -0700

    Added loadEngine functionality

--- Functions/loadEngine.m
+++ Functions/loadEngine.m
@@ -5,7 +5,24 @@ function aircraft = loadEngine(inputs,aircraft)
 %   aircraft. For now I imagine a large if-elseif structure will suffice,
 %   but this may change in the future if we get a large amount of
 %   propulsion equiptment in our database. 
+%
+%%%
 
+batteryName = inputs.batteryInput;
+motorName = inputs.motorInput;
+ESCName = inputs.ESCInput;
+propellerName = inputs.propellerInput;
+servoName = inputs.servoInput;
+
+
+
+%% Packaging
+
+aircraft.engine.battery = batteryName;
+aircraft.engine.motor = motorName;
+aircraft.engine.ESC = ESCName;
+aircraft.engine.propeller = propellerName;
+aircraft.engine.servo = servoName;
 
 
 end
