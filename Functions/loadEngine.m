49ef1cd9a9d60db28399ca3bd6ee9b484360cb34
Nathen Carey <nathen@jetzero.aero>
2025-06-20 07:37:56 -0700

    Added selection structure for propulsion system inputs within loadEngine

--- Functions/loadEngine.m
+++ Functions/loadEngine.m
@@ -15,14 +15,77 @@ propellerName = inputs.propellerInput;
 servoName = inputs.servoInput;
 
 
+%% Selection Structure
+
+% Battery 
+if strcmp(batteryName,'LiPo 2200mAh 45-60C')
+    batteryWeight = 0.3831;   % pounds 
+    capacity = 2200;   % mAh
+    maxCRating = 60;   % C
+end
+
+% Motor 
+if strcmp(motorName,'Cobra C-2808/16 (1780)')
+    motorWeight = 0.1775;   % pounds
+    KV = 1780;   % rpm/V
+    motorLength = 0; 
+    motorDiameter = 0; 
+end
+
+% ESC
+if strcmp(ESCName,'max 50A')
+    current = 50;   % Amp
+    ESCWeight = 0.1433;   % pounds
+end
+
+% Propeller 
+if strcmp(propellerName,'APC Electric E 8x5')
+    propellerWeight = 0;   % pounds
+    propDiam = 8;   % propeller diameter (inches)
+    propPitch = 5;   % propeller pitch (inches)
+end
+
+% Servos 
+
 
 %% Packaging
 
-aircraft.engine.battery = batteryName;
-aircraft.engine.motor = motorName;
-aircraft.engine.ESC = ESCName;
-aircraft.engine.propeller = propellerName;
-aircraft.engine.servo = servoName;
+% ENGINE
+aircraft.engine.batteryName = batteryName;
+aircraft.engine.motorName = motorName;
+aircraft.engine.ESCName = ESCName;
+aircraft.engine.propellerName = propellerName;
+aircraft.engine.servoName = servoName;
+
+% battery
+aircraft.engine.battery.weight = batteryWeight;
+aircraft.engine.battery.capacity = capacity;
+aircraft.engine.battery.maxCRating = maxCRating;
+
+% motor 
+aircraft.engine.motor.weight = motorWeight;
+aircraft.engine.motor.KV = KV;
+aircraft.engine.motor.length = motorLength;
+aircraft.engine.motor.diameter = motorDiameter;
+
+% ESC 
+aircraft.engine.ESC.current = current;
+aircraft.engine.ESC.weight = ESCWeight;
+
+% propeller
+aircraft.engine.propeller.weight = propellerWeight;
+aircraft.engine.propeller.diameter = propDiam;
+aircraft.engine.propeller.pitch = propPitch;
+
+% servos
+
+
+% WEIGHTS 
+aircraft.weight.sum.battery = batteryWeight;
+aircraft.weight.sum.ESC = ESCWeight;
+aircraft.weight.sum.motor = motorWeight;
+aircraft.weight.sum.propeller = propellerWeight;
+
 
 
 end
