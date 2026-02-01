4f137dd48da949b4390289802f8cb26b0fb5a4ba
Nathen Carey <nathen@jetzero.aero>
2026-02-01 03:28:29 -0500

    Started the shell of runMission. This is where force summation mission simulation will occur in the future

---
+++ Functions/runMission.m
@@ -0,0 +1,27 @@
+function [aircraft,mission] = runMission(aircraft,mission)
+% [aircraft,mission] = runMission(aircraft,mission)
+%
+%   - This function will input the finalized aircraft structure and mission
+%   specifics and simulate the flight of the aircraft. 
+%
+%
+%%%%
+
+
+%% Takeoff
+
+
+%% Climb 
+
+
+%% Cruise 
+
+
+%% Decent
+
+
+%% Landing
+
+
+
+end
